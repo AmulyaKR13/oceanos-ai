d9bfbf08391c2f2d2eb4c32e13bfa01a8e559d2c
Nishant <nishant2024rde@gmail.com>
2026-04-01 12:09:11 +0530

    my cmt nis

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
-//My comit
+//My comit Nishant
 function App() {
   const [count, setCount] = useState(0)
 
